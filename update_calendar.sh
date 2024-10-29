5945c8c51438c68c0f1b44236fa93eab8a791e2f
Timoti Fregni <timotif@gmail.com>
2024-10-29 13:05:34 +0100

    reading environment from .env for local testing

--- update_calendar.sh
+++ update_calendar.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 CALENDAR_PATH=geca_calendar/
 
+# Load environment variables from .env file
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+fi
+
 python ${CALENDAR_PATH}calendar_updater.py
