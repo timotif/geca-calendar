1eeda10951af861f6fed57feafa308ed0140d86e
Timoti Fregni <timotif@gmail.com>
2024-11-14 20:37:39 +0100

    gracious exit on Keyboard Interrupt

--- geca_calendar/calendar_updater.py
+++ geca_calendar/calendar_updater.py
@@ -1,16 +1,20 @@
 import os
+import dotenv
 from notion_interface import read_database, create_calendar
 from logging_config import logger
 
 ENV_FILE = ".env"
 
-import dotenv
 def main():
     if not os.getenv("SECRET_KEY") or not os.getenv("NOTION_TOKEN") or not os.getenv("NOTION_DB_ID"):
         logger.info("Environment loaded") if dotenv.load_dotenv(ENV_FILE) else logger.info("Environment not loaded")
-
-    data = read_database(database_id=os.environ.get("NOTION_DB_ID"), token=os.environ.get("NOTION_TOKEN"))
-    create_calendar(data, save_to_json=True if os.getenv("DEBUG") else False)
+    try:
+        data = read_database(database_id=os.environ.get("NOTION_DB_ID"), token=os.environ.get("NOTION_TOKEN"))
+        create_calendar(data, save_to_json=True if int(os.getenv("DEBUG")) else False)
+        logger.info("Calendar updated")
+    except KeyboardInterrupt:
+        logger.info("Exiting")
+        exit(0)
 
 if __name__ == "__main__":
     main()
