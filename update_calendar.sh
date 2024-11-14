4af3d8fda22abfe45a13a2ef00c5447e5b0d96e0
Timoti Fregni <timotif@gmail.com>
2024-11-14 20:39:20 +0100

    cosmetic

--- update_calendar.sh
+++ update_calendar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-CALENDAR_PATH=geca_calendar/
+CALENDAR_PATH=geca_calendar
 
 # Load environment variables from .env file
 if [ -f .env ]; then
     export $(cat .env | xargs)
 fi
 
-python ${CALENDAR_PATH}calendar_updater.py
+python ${CALENDAR_PATH}/calendar_updater.py
