110b12f1d175eb00ea7d40fca0a658546c0fcb0d
Timoti Fregni <timotif@gmail.com>
2025-01-02 18:53:39 +0100

    new standalone version of calendar updater using the new architecture

--- app/calendar_updater.py
+++ app/calendar_updater.py
@@ -1,20 +1,22 @@
 import os
 import dotenv
-from notion_interface import read_notion_database, fetch_projects, create_calendar
 from logging_config import logger
+from notion_client import NotionDataSource
+from calendar_generator import ICSCalendarGenerator
 
 ENV_FILE = ".env"
 
 def main():
-	if not os.getenv("SECRET_KEY") or not os.getenv("NOTION_TOKEN") or not os.getenv("NOTION_DB_ID"):
+	if not os.getenv("NOTION_TOKEN") or not os.getenv("NOTION_DB_ID"):
 		logger.info("Environment loaded") if dotenv.load_dotenv(ENV_FILE) else logger.info("Environment not loaded")
 	try:
-		data = read_notion_database(database_id=os.environ.get("NOTION_DB_ID"))
-		project_list = fetch_projects(data)
-		create_calendar(project_list, save_to_json=True if os.getenv("DEBUG") == '1' else False)
+		notion = NotionDataSource(os.getenv("NOTION_TOKEN"), os.getenv("NOTION_DB_ID"))
+		ics = ICSCalendarGenerator()
+		data = notion.fetch_data()
+		ics.generate(data, 'geca2425.ics')
 		logger.info("Calendar updated")
 	except KeyboardInterrupt:
-		logger.info("Exiting")
+		logger.info("Exiting...")
 		exit(0)
 
 if __name__ == "__main__":
