ee0128ced818528d90cd641b8d5de7c9763e0212
Timoti Fregni <timotif@gmail.com>
2025-01-03 12:06:47 +0100

    turned date_start and date_end to str as they were treated as such in the rest of the program

--- app/data_transfer_objects.py
+++ app/data_transfer_objects.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
-from datetime import datetime
 
 @dataclass
 class ProjectDTO:
 	"""Data Transfer Object for Project"""
 	id: str
 	name: str
-	date_start: datetime
-	date_end: datetime
+	date_start: str
+	date_end: str
 	url: str = ""
 	seating: str = ""
