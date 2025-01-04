cda532ce430e77b0410f5e383c4394e151fbe54f
Timoti Fregni <timotif@gmail.com>
2025-01-04 12:53:54 +0100

    JS selector instead of checkbox

--- app/static/js/projects.js
+++ app/static/js/projects.js
@@ -1,29 +1,37 @@
-document.getElementById('loader').style.display = 'none';
 document.addEventListener('DOMContentLoaded', async () => {
-const btn = document.getElementsByClassName('submit-list').disabled = true;
-document.getElementById('loader').style.display = 'block';
-// Fetch projects
-const response = await fetch('/fetch_projects');
-const data = await response.json();
-const projectsList = document.getElementById('projects-list');
+	const btn = document.getElementsByClassName('submit-list').disabled = true;
+	document.getElementById('loader').style.display = 'block';
 
-for (const p of data) {
-	li = document.createElement('li');
-	li.className = "list-group-item d-flex justify-content-between align-items-center";
+	// Fetch projects
+	const response = await fetch('/fetch_projects');
+	const data = await response.json();
+	const projectsList = document.getElementById('projects-list');
 
-	checkbox = document.createElement('input');
-	checkbox.type = 'checkbox';
-	checkbox.name = 'selected_projects';
-	checkbox.value = p.id;
-	checkbox.className = 'form-check-input me-2';
-	
-	li.appendChild(checkbox);
-	li.appendChild(document.createTextNode(p.name));
-	projectsList.appendChild(li);
-}
-const projects = document.getElementById('projects-form');
-projects.appendChild(projectsList);
-document.getElementById('loader').style.display = 'none';
-document.getElementById('projects').style.display = 'block';
-btn.disabled = false;
+	for (const p of data) {
+		const li = document.createElement('li');
+		li.className = "list-group-item d-flex justify-content-between align-items-center";
+		li.dataset.id = p.id;
+
+		// Create hidden input to store selected projects
+		const hiddenInput = document.createElement('input');
+		hiddenInput.type = 'hidden';
+		hiddenInput.name = 'selected_projects';
+		hiddenInput.value = p.id;
+		hiddenInput.disabled = true;
+		
+		li.appendChild(hiddenInput);
+		li.appendChild(document.createTextNode(p.name));
+
+		// Add event listener to each project
+		li.addEventListener('click', () => {
+			li.classList.toggle('selected');
+			hiddenInput.disabled = !hiddenInput.disabled;
+		});
+		projectsList.appendChild(li);
+	}
+	const projects = document.getElementById('projects-form');
+	projects.appendChild(projectsList);
+	document.getElementById('loader').style.display = 'none';
+	document.getElementById('projects').style.display = 'block';
+	btn.disabled = false;
 })
